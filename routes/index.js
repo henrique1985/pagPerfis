d65c7f74842756f8f52ac23f20165feb51abad2e
Henrique Florencio <henrique.otf@gmail.com>
2018-08-13 18:01:44 -0300

    Construir HTML e CSS.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Segunda coe' });
+});
+
+router.get('/helicopterson', function(req, res, next) {
+  res.render('bio');
 });
 
 module.exports = router;
